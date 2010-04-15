69f5424cce1d5e016703b7d4bff0e12e09bf8f76
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-04-15 10:27:15 -0500

    probably good to get that patterns to look in the right module :|

--- felix/patterns.py
+++ felix/patterns.py
@@ -1,6 +1,6 @@
 from nappingcat.patterns import patterns
 
-cmdpatterns = patterns('auth.handlers', 
+cmdpatterns = patterns('felix.handlers', 
     (r'add-user (?P<username>[\w\.\-]+)', 'add_user'),
     (r'add-key-to-user (?P<username>[\w\.\-]+)', 'add_key_to_user'),
     (r'add-permission (?P<username>[\w\.\-]+) \'(?P<permission>.*)\'', 'add_permission'),
